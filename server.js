dd4f498ca64eed6a1aabdc0f4c8e3450de15f630
ed louise amor <ed.amor@xlog.com>
2021-01-19 00:49:06 +0800

    include find match in multiple word names

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const database = require('./data.json')
-
-
+const database = require('./data.json');
 
 app.get('/api/suggest', (req, res) => {
 
   const response = [];
-  
+
   try {
     const { q } = req.query;
-
+    
     // Only check the state field if query string length is less than or equal to 2
     if (q.length <= 2) {
       database.forEach(item => {
@@ -23,32 +21,48 @@ app.get('/api/suggest', (req, res) => {
       })
 
       // If initial search found nothing, search query string in name field
-      if (response.length === 0) {
-        database.forEach(item => {
-          if (response.length >= 5) {
-            return;
-          }
-          if (item.name.toUpperCase().startsWith(q.toUpperCase())) {
-            response.push(item)
-          }
-        })
-      }
-
-    // If query string length is greater than 2, then no need to search state field
-    // Will search name field instead
-    } else {
       database.forEach(item => {
         if (response.length >= 5) {
           return;
         }
         if (item.name.toUpperCase().startsWith(q.toUpperCase())) {
-          response.push(item)
+          response.unshift(item)
         }
       })
     }
+
+    // If query.string.length is greater than 2,
+    // then no need to search state field
+    // Will search name field instead
+    database.forEach(item => {
+      if (response.length < 5) {
+        if (item.name.toUpperCase().startsWith(q.toUpperCase())) {
+
+          // Put first word match in item.name 
+          // to the start of the list
+          response.unshift(item);   
+          return;
+        }
+
+        // Split multiple words but skip first word
+        let withSpaces = item.name.toUpperCase().split(" ").slice(1); 
+        let upperQ = q.toUpperCase();
+        let matches = 0;
+        withSpaces.forEach(word => {
+          if (word.startsWith(upperQ)) {
+            matches++
+          }
+        })
+        if (matches > 0) {
+          response.push(item)
+        }
+      }
+    })
   } catch (error) {
     return res.status(400).json({ error: error.toString() });
   }
+
+  // Return response[]
   return res.send(response)
 });
 
